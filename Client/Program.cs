715fc70441ee370abdc9138aad4076c3ee4a493d
ercheng2 <ercheng2@github.com>
2026-05-26 00:18:56 +0800

    feat: switch to .NET 8 with self-contained single-file exe

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,14 +8,23 @@ namespace VisualControl.Client
         [STAThread]
         static void Main(string[] args)
         {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += (s, e) =>
+            {
+                MessageBox.Show($"UI异常: {e.Exception.Message}\n\n{e.Exception.StackTrace}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                var ex = e.ExceptionObject as Exception;
+                MessageBox.Show($"致命异常: {ex?.Message}\n\n{ex?.StackTrace}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             bool autoStart = Array.IndexOf(args, "--auto") >= 0;
             string serverIp = "127.0.0.1";
             int serverPort = 9600;
-
-            // 解析命令行参数
             for (int i = 0; i < args.Length - 1; i++)
             {
                 if (args[i] == "--server") serverIp = args[i + 1];
--- Server/Program.cs
+++ Server/Program.cs
@@ -8,6 +8,17 @@ namespace VisualControl.Server
         [STAThread]
         static void Main()
         {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += (s, e) =>
+            {
+                MessageBox.Show($"UI异常: {e.Exception.Message}\n\n{e.Exception.StackTrace}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                var ex = e.ExceptionObject as Exception;
+                MessageBox.Show($"致命异常: {ex?.Message}\n\n{ex?.StackTrace}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Forms.MainForm());
